cc2f0da92a251e0992798d699b8d05235e341f83
Onair-santa <42511409+Onair-santa@users.noreply.github.com>
2025-09-20 01:21:42 +0500

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,6 +1,6 @@
 class Presets(object):
     WELCOME_TEXT = "**Hi.** **{}**\n\n`Send me a message. I'll reply shortly.` 😉"
-    USER_DETAILS = "🔹<code>Name:</code> <b>{} {}</b>\n       <code>Id:</code>      <code>{}</code>\n       <code>Uname:</code>@{}"
+    USER_DETAILS = "⭐️ <code>Name:</code> <b>{} {}</b>\n       <code>Id:</code>      <code>{}</code>\n       <code>Uname:</code>@{}"
     PM_TXT_ATT = "🔹 <code>#</code> <code>{}</code>\n    <b>{}</b>\n    @{}\n\n{}"
     PM_MED_ATT = "🔹 <code>#</code> <code>{}</code>\n    <b>{}</b>\n    @{}"
     PM_STICKER_ATT = "🔹 <code>#</code> <code>{}</code>\n    <b>{}</b>\n    @{}"
